5d8726a00dca9e267d5e80318eed2d151ae0bbd1
anjanidwhr <159592844+anjanidwhr@users.noreply.github.com>
2024-08-21 15:19:52 +0700

    Add files via upload

---
+++ cobaa1/src/main/java/com/mycompany/cobaa1/Cobaa1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cobaa1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Cobaa1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
